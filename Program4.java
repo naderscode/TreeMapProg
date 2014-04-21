4755fd160153c68ef17d7045790b50c410ac7a88
naderscode <naderscode@gmail.com>
2014-04-21 16:00:36 -0400

    edit file

--- Program4.java
+++ Program4.java
@@ -1,14 +1,14 @@
 /**
-Date: 3/31/2006
-Program created by Nader K for CIS 2353 .
-Filename: Program4.java
-This application processes the two data files sections.txt and register.txt,
-to show which classes students have signed up for,for the current semester.
-The program will report the list of available sections, then report the name 
-of each student and the sections they registered for.
-The program will also report errors when there are duplicate sections, 
-any attempts to register for a section that does not exist, or when a student tries 
-to register again in  the same section.
+    Date: 3/31/2006
+    Program created by Nader K for CIS 2353 .
+    Filename: Program4.java
+    This application processes the two data files sections.txt and register.txt,
+    to show which classes students have signed up for,for the current semester.
+    The program will report the list of available sections, then report the name 
+    of each student and the sections they registered for.
+    The program will also report errors when there are duplicate sections, 
+    any attempts to register for a section that does not exist, or when a student tries 
+    to register again in  the same section.
 */
 
 import java.io.*;
